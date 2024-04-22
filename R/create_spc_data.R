160cc8fe81307cec7734baf5f25aecc93593532b
singhdevang <devang.singh@wales.nhs.uk>
2024-04-22 10:13:02 +0100

    now includes trend column

--- R/create_spc_data.R
+++ R/create_spc_data.R
@@ -1,11 +1,15 @@
 #' Create SPC Data
 #'
-#' This function creates a Statistical Process Control chart data and ensures LCL values are not negative.
+#' This function creates a Statistical Process Control (SPC) chart data and ensures LCL values are not negative.
+#' It includes a 'shift' column indicating whether the data point's value is below or equal to the center line (CL).
+#' Additionally, a 'trend' column is added to show whether each subsequent value of 'y' is greater than its predecessor.
+#' Values in 'trend' are TRUE if the value is increasing, NA if constant, and FALSE if decreasing.
+#'
 #' @param data Data frame containing the data
 #' @param date_col Name of the date column
 #' @param value_col Name of the value column
 #' @param chart_type Type of SPC chart to create
-#' @return Returns a data frame with SPC data including adjusted LCL values.
+#' @return Returns a data frame with SPC data including adjusted LCL values, 'shift' and 'trend' status columns.
 #' @export
 #' @importFrom qicharts2 qic
 #' @importFrom lubridate parse_date_time
@@ -14,7 +18,7 @@
 #'                    value = rnorm(30, 100, 15))
 #' create_spc_data(data, 'date', 'value', 'xbar')
 create_spc_data <- function(data, date_col, value_col, chart_type) {
-  # Try to parse the date column using common date formats, now using uppercase for time components
+  # Try to parse the date column using common date formats
   data[[date_col]] <- parse_date_time(data[[date_col]],
                                       orders = c("ymd_HMS", "ymd_HM", "ymd_H", "ymd",
                                                  "mdy_HMS", "mdy_HM", "mdy_H", "mdy",
@@ -28,11 +32,20 @@ create_spc_data <- function(data, date_col, value_col, chart_type) {
   chart_data <- chart$data
   chart_data$lcl <- pmax(chart_data$lcl, 0)
 
-  # Add shift column where shift is TRUE if y is less than cl
-  chart_data$shift <- chart_data$y < chart_data$cl
+  # Add shift column where shift is TRUE if y < cl, NA if y == cl
+  chart_data$shift <- ifelse(chart_data$y == chart_data$cl, NA, chart_data$y < chart_data$cl)
+
+  # Calculate trend column comparing consecutive y values
+  chart_data$trend <- c(NA, ifelse(diff(chart_data$y) > 0, TRUE, ifelse(diff(chart_data$y) == 0, NA, FALSE)))
+
+  # Set the first entry of trend to be the same as the second entry
+  if (!is.na(chart_data$trend[2])) {
+    chart_data$trend[1] <- chart_data$trend[2]
+  } else {
+    # This handles the case if the second value is NA; the first value then defaults to FALSE
+    chart_data$trend[1] <- FALSE
+  }
 
   # Return the updated data frame
   return(chart_data)
 }
-
-
