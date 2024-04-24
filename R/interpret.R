16523c88361444d24a81c502ddc7f9d8b7ce7af1
singhdevang <devang.singh@wales.nhs.uk>
2024-04-24 10:43:20 +0100

    namespace updated

--- R/interpret.R
+++ R/interpret.R
@@ -1,13 +1,31 @@
-#' Interpret SPC Data
+#' Interpret SPC Data for Special Cause Variations
 #'
-#' This function interprets the SPC data to detect special cause variations such as shifts, trends,
-#' 15+ consecutive points, and sigma signals. It outputs a table summarizing these findings with
-#' the range of occurrences.
+#' This function analyzes SPC data generated from `create_spc_data` to detect special cause variations
+#' such as shifts, trends, and excessive deviations. It identifies significant events based on predefined
+#' criteria and displays these events in a tabular format indicating the type of variation and its
+#' duration or specific occurrence point.
 #'
-#' @param data A data frame containing the SPC data including at least the columns 'x' for dates,
-#'        'y' for the measurement values, and additional calculated columns for analysis.
-#' @return A data frame with columns "Special Cause Variation" and "Duration" indicating the type
-#'         of variation and the corresponding range of dates.
+#' @param data A data frame containing SPC data, expected to include the columns `x` for the time or date,
+#'   `shift`, `trend`, `fifteen_more`, and `sigma.signal`, which are logical vectors indicating where these
+#'   conditions are met.
+#'
+#' @return Returns a data frame with two columns: `SpecialCauseVariation`, which describes the type of
+#'   variation detected, and `Duration`, which provides the duration or the specific point of occurrence
+#'   for sigma signals. For interactive sessions, the results are also displayed using `View` or `print`.
+#'
+#' @importFrom stats rle
+#' @examples
+#' # Assuming 'spc_data' is a dataframe obtained from `create_spc_data` function
+#' spc_data <- data.frame(
+#'   x = seq(as.Date("2020-01-01"), by = "month", length.out = 24),
+#'   shift = c(rep(FALSE, 10), rep(TRUE, 8), rep(FALSE, 6)),
+#'   trend = c(rep(FALSE, 5), rep(TRUE, 10), rep(FALSE, 9)),
+#'   fifteen_more = c(rep(FALSE, 15), rep(TRUE, 5), rep(FALSE, 4)),
+#'   sigma.signal = c(rep(FALSE, 20), rep(TRUE, 4))
+#' )
+#' interpreted_data <- interpret(spc_data)
+#' print(interpreted_data)
+#'
 #' @export
 interpret <- function(data) {
   # Helper function to compute RLE while skipping NAs, focusing on the specific column
